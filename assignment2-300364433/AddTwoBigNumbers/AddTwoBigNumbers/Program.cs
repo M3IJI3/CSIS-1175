4ae857b85146ae6b5867a13b7bc2fb90993b231c
M3IJI3 <majorangeee@gmail.com>
2022-11-14 19:13:31 -0800

    update files

--- assignment2-300364433/AddTwoBigNumbers/AddTwoBigNumbers/Program.cs
+++ assignment2-300364433/AddTwoBigNumbers/AddTwoBigNumbers/Program.cs
@@ -16,7 +16,7 @@ namespace AddTwoBigNumbers
         static void Main(string[] args)
         {
             string s1 = "6754356756348786543256456789876545324156453466787654565545676543122234567890987654321234567898765432";
-            string s2 = "1235467908767890987234567098765234567898765213456909876756453253467898764562156789345678909817265431";
+            string s2 = "9235467908767890987234567098765234567898765213456909876756453253467898764562156789345678909817265431";
             // WriteLine(AddTwoBigIntegers(s1, s2));
             WriteLine("#1 number: {0}", s1);
             WriteLine("#2 number: {0}", s2);
@@ -24,7 +24,7 @@ namespace AddTwoBigNumbers
 
             WriteLine("\t{0}\n + \t{1}", s1, s2);
             WriteLine("".PadLeft(SIZE + 8, '-'));
-            WriteLine("\t{0}", AddTwoBigIntegers(s1, s2));
+            WriteLine("       {0}", AddTwoBigIntegers(s1, s2));
 
             ReadKey();
         }
@@ -61,7 +61,7 @@ namespace AddTwoBigNumbers
                 int sum = num1[i] + num2[i] + carry;
                 result[i] = sum % 10;
                 carry = sum / 10;
-
+  
                 if (i == 0)
                 {
                     result[i] = sum;
