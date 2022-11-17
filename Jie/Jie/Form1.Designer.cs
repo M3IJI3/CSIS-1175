82de4126b9cf3c56bd7301a4e7cf193851691093
M3IJI3 <majorangeee@gmail.com>
2022-11-16 23:08:36 -0800

    update files

---
+++ Jie/Jie/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace jie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOne
+            // 
+            this.btnOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOne.Location = new System.Drawing.Point(12, 12);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(80, 80);
+            this.btnOne.TabIndex = 0;
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTwo.Location = new System.Drawing.Point(98, 12);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(80, 80);
+            this.btnTwo.TabIndex = 1;
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnThree
+            // 
+            this.btnThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThree.Location = new System.Drawing.Point(184, 12);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(80, 80);
+            this.btnThree.TabIndex = 2;
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // btnFour
+            // 
+            this.btnFour.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFour.Location = new System.Drawing.Point(12, 98);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(80, 80);
+            this.btnFour.TabIndex = 3;
+            this.btnFour.UseVisualStyleBackColor = true;
+            this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+            // 
+            // btnFive
+            // 
+            this.btnFive.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFive.Location = new System.Drawing.Point(98, 98);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(80, 80);
+            this.btnFive.TabIndex = 4;
+            this.btnFive.UseVisualStyleBackColor = true;
+            this.btnFive.Click += new System.EventHandler(this.btnFive_Click);
+            // 
+            // btnSix
+            // 
+            this.btnSix.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSix.Location = new System.Drawing.Point(184, 98);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(80, 80);
+            this.btnSix.TabIndex = 5;
+            this.btnSix.UseVisualStyleBackColor = true;
+            this.btnSix.Click += new System.EventHandler(this.btnSix_Click);
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeven.Location = new System.Drawing.Point(12, 184);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(80, 80);
+            this.btnSeven.TabIndex = 6;
+            this.btnSeven.UseVisualStyleBackColor = true;
+            this.btnSeven.Click += new System.EventHandler(this.btnSeven_Click);
+            // 
+            // btnEight
+            // 
+            this.btnEight.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEight.Location = new System.Drawing.Point(98, 184);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(80, 80);
+            this.btnEight.TabIndex = 7;
+            this.btnEight.UseVisualStyleBackColor = true;
+            this.btnEight.Click += new System.EventHandler(this.btnEight_Click);
+            // 
+            // btnNine
+            // 
+            this.btnNine.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNine.Location = new System.Drawing.Point(184, 184);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(80, 80);
+            this.btnNine.TabIndex = 8;
+            this.btnNine.UseVisualStyleBackColor = true;
+            this.btnNine.Click += new System.EventHandler(this.btnNine_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer.Location = new System.Drawing.Point(68, 278);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(144, 25);
+            this.lblPlayer.TabIndex = 10;
+            this.lblPlayer.Text = "Player \'1\' turn";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(98, 322);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(80, 38);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "New Game";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 378);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblPlayer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnOne);
+            this.Name = "Form1";
+            this.Text = "TicTac";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Tictac/jie/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jie
+{
+    public partial class Form1 : Form
+    {
+        string promption = "Player '1' turn";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            if(lblPlayer.Text == promption)
+            {
+                btnOne.Text = "1";
+                lblPlayer.Text = "Player '0' turn";
+            }
+            else
+            {
+                btnOne.Text = "0";
+                lblPlayer.Text = "Player '1' turn";
+            }
+            btnOne.Enabled = false;
+
+            Rule();
+            
+        }
+
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer.Text == promption)
+            {
+                btnTwo.Text = "1";
+                lblPlayer.Text = "Player '0' turn";
+            }
+            else
+            {
+                btnTwo.Text = "0";
+                lblPlayer.Text = "Player '1' turn";
+            }
+            btnTwo.Enabled = false;
+            Rule();
+        }
+
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer.Text == promption)
+            {
+                btnThree.Text = "1";
+                lblPlayer.Text = "Player '0' turn";
+            }
+            else
+            {
+                btnThree.Text = "0";
+                lblPlayer.Text = "Player '1' turn";
+            }
+            btnThree.Enabled = false;
+            Rule();
+            
+        }
+
+        private void btnFour_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer.Text == promption)
+            {
+                btnFour.Text = "1";
+                lblPlayer.Text = "Player '0' turn";
+            }
+            else
+            {
+                btnFour.Text = "0";
+                lblPlayer.Text = "Player '1' turn";
+            }
+            btnFour.Enabled = false;
+            Rule();
+        }
+
+        private void btnFive_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer.Text == promption)
+            {
+                btnFive.Text = "1";
+                lblPlayer.Text = "Player '0' turn";
+            }
+            else
+            {
+                btnFive.Text = "0";
+                lblPlayer.Text = "Player '1' turn";
+            }
+            btnFive.Enabled = false;
+            Rule();
+        }
+
+        private void btnSix_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer.Text == promption)
+            {
+                btnSix.Text = "1";
+                lblPlayer.Text = "Player '0' turn";
+            }
+            else
+            {
+                btnSix.Text = "0";
+                lblPlayer.Text = "Player '1' turn";
+            }
+            btnSix.Enabled = false;
+            Rule();
+        }
+
+        private void btnSeven_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer.Text == promption)
+            {
+                btnSeven.Text = "1";
+                lblPlayer.Text = "Player '0' turn";
+            }
+            else
+            {
+                btnSeven.Text = "0";
+                lblPlayer.Text = "Player '1' turn";
+            }
+            btnSeven.Enabled = false;
+            Rule();
+        }
+
+        private void btnEight_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer.Text == promption)
+            {
+                btnEight.Text = "1";
+                lblPlayer.Text = "Player '0' turn";
+            }
+            else
+            {
+                btnEight.Text = "0";
+                lblPlayer.Text = "Player '1' turn";
+            }
+            btnEight.Enabled = false;
+            Rule();
+        }
+
+        private void btnNine_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer.Text == promption)
+            {
+                btnNine.Text = "1";
+                lblPlayer.Text = "Player '0' turn";
+            }
+            else
+            {
+                btnNine.Text = "0";
+                lblPlayer.Text = "Player '1' turn";
+            }
+            btnNine.Enabled = false;
+            Rule();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            lblPlayer.Text = promption;
+
+            btnOne.Enabled = true;
+            btnTwo.Enabled = true;
+            btnThree.Enabled = true;
+            btnFour.Enabled = true;
+            btnFive.Enabled = true;
+            btnSix.Enabled = true;
+            btnSeven.Enabled = true;
+            btnEight.Enabled = true;
+            btnNine.Enabled = true;
+
+            btnOne.Text = "";
+            btnTwo.Text = "";
+            btnThree.Text = "";
+            btnFour.Text = "";
+            btnFive.Text = "";
+            btnSix.Text = "";
+            btnSeven.Text = "";
+            btnEight.Text = "";
+            btnNine.Text = "";
+        }
+
+        // [0,0,0],[0,0,0],[0,0,0]
+        public void Rule()
+        {
+            if ((btnOne.Text == "1" && btnTwo.Text == "1" && btnThree.Text == "1") ||
+                (btnOne.Text == "1" && btnFour.Text == "1" && btnSeven.Text == "1") ||
+                (btnOne.Text == "1" && btnFive.Text == "1" && btnNine.Text == "1") ||
+                (btnTwo.Text == "1" && btnFive.Text == "1" && btnEight.Text == "1") ||
+                (btnThree.Text == "1" && btnSix.Text == "1" && btnNine.Text == "1") ||
+                (btnThree.Text == "1" && btnFive.Text == "1" && btnSeven.Text == "1") ||
+                (btnFour.Text == "1" && btnFive.Text == "1" && btnSix.Text == "1") ||
+                (btnSeven.Text == "1" && btnEight.Text =="1" && btnNine.Text == "1"))
+            {
+                MessageBox.Show("Player '1' win!");
+                Reset();
+            }
+
+            if((btnOne.Text == "0" && btnTwo.Text == "0" && btnThree.Text == "0") ||
+                (btnOne.Text == "0" && btnFour.Text == "0" && btnSeven.Text == "0") ||
+                (btnOne.Text == "0" && btnFive.Text == "0" && btnNine.Text == "0") ||
+                (btnTwo.Text == "0" && btnFive.Text == "0" && btnEight.Text == "0") ||
+                (btnThree.Text == "0" && btnSix.Text == "0" && btnNine.Text == "0") ||
+                (btnThree.Text == "0" && btnFive.Text == "0" && btnSeven.Text == "0") ||
+                (btnFour.Text == "0" && btnFive.Text == "0" && btnSix.Text == "0") ||
+                (btnSeven.Text == "0" && btnEight.Text == "0" && btnNine.Text == "0"))
+            {
+                MessageBox.Show("Player '0' win!");
+                Reset();
+            }
+        }
+
+    }
+}
