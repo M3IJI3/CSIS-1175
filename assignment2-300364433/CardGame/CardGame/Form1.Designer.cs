c4726afa968b62b1375c72e3a30e621e28833c9b
M3IJI3 <majorangeee@gmail.com>
2022-11-18 19:57:15 -0800

    update files

---
+++ assignment2-300364433/CardGame/CardGame/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace CardGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCptOne = new System.Windows.Forms.Button();
+            this.btnCptTwo = new System.Windows.Forms.Button();
+            this.btnCptThree = new System.Windows.Forms.Button();
+            this.btnCptFour = new System.Windows.Forms.Button();
+            this.btnPlayerOne = new System.Windows.Forms.Button();
+            this.btnPlayerTwo = new System.Windows.Forms.Button();
+            this.btnPlayerThree = new System.Windows.Forms.Button();
+            this.btnPlayerFour = new System.Windows.Forms.Button();
+            this.btnBet = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Computer\'s cards";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Player\'s cards";
+            // 
+            // btnCptOne
+            // 
+            this.btnCptOne.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCptOne.Location = new System.Drawing.Point(18, 80);
+            this.btnCptOne.Name = "btnCptOne";
+            this.btnCptOne.Size = new System.Drawing.Size(80, 80);
+            this.btnCptOne.TabIndex = 2;
+            this.btnCptOne.UseVisualStyleBackColor = true;
+            // 
+            // btnCptTwo
+            // 
+            this.btnCptTwo.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCptTwo.Location = new System.Drawing.Point(126, 80);
+            this.btnCptTwo.Name = "btnCptTwo";
+            this.btnCptTwo.Size = new System.Drawing.Size(80, 80);
+            this.btnCptTwo.TabIndex = 3;
+            this.btnCptTwo.UseVisualStyleBackColor = true;
+            // 
+            // btnCptThree
+            // 
+            this.btnCptThree.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCptThree.Location = new System.Drawing.Point(236, 80);
+            this.btnCptThree.Name = "btnCptThree";
+            this.btnCptThree.Size = new System.Drawing.Size(80, 80);
+            this.btnCptThree.TabIndex = 4;
+            this.btnCptThree.UseVisualStyleBackColor = true;
+            // 
+            // btnCptFour
+            // 
+            this.btnCptFour.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCptFour.Location = new System.Drawing.Point(350, 80);
+            this.btnCptFour.Name = "btnCptFour";
+            this.btnCptFour.Size = new System.Drawing.Size(80, 80);
+            this.btnCptFour.TabIndex = 5;
+            this.btnCptFour.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayerOne
+            // 
+            this.btnPlayerOne.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayerOne.Location = new System.Drawing.Point(18, 274);
+            this.btnPlayerOne.Name = "btnPlayerOne";
+            this.btnPlayerOne.Size = new System.Drawing.Size(80, 80);
+            this.btnPlayerOne.TabIndex = 6;
+            this.btnPlayerOne.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayerTwo
+            // 
+            this.btnPlayerTwo.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayerTwo.Location = new System.Drawing.Point(126, 274);
+            this.btnPlayerTwo.Name = "btnPlayerTwo";
+            this.btnPlayerTwo.Size = new System.Drawing.Size(80, 80);
+            this.btnPlayerTwo.TabIndex = 7;
+            this.btnPlayerTwo.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayerThree
+            // 
+            this.btnPlayerThree.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayerThree.Location = new System.Drawing.Point(236, 274);
+            this.btnPlayerThree.Name = "btnPlayerThree";
+            this.btnPlayerThree.Size = new System.Drawing.Size(80, 80);
+            this.btnPlayerThree.TabIndex = 8;
+            this.btnPlayerThree.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayerFour
+            // 
+            this.btnPlayerFour.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayerFour.Location = new System.Drawing.Point(350, 274);
+            this.btnPlayerFour.Name = "btnPlayerFour";
+            this.btnPlayerFour.Size = new System.Drawing.Size(80, 80);
+            this.btnPlayerFour.TabIndex = 9;
+            this.btnPlayerFour.UseVisualStyleBackColor = true;
+            // 
+            // btnBet
+            // 
+            this.btnBet.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBet.Location = new System.Drawing.Point(105, 392);
+            this.btnBet.Name = "btnBet";
+            this.btnBet.Size = new System.Drawing.Size(101, 46);
+            this.btnBet.TabIndex = 10;
+            this.btnBet.Text = "Bet";
+            this.btnBet.UseVisualStyleBackColor = true;
+            this.btnBet.Click += new System.EventHandler(this.btnBet_Click);
+            // 
+            // btnDone
+            // 
+            this.btnDone.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDone.Location = new System.Drawing.Point(236, 392);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(101, 46);
+            this.btnDone.TabIndex = 11;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(138, 464);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(163, 46);
+            this.btnReset.TabIndex = 12;
+            this.btnReset.Text = "New Game";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 545);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.btnBet);
+            this.Controls.Add(this.btnPlayerFour);
+            this.Controls.Add(this.btnPlayerThree);
+            this.Controls.Add(this.btnPlayerTwo);
+            this.Controls.Add(this.btnPlayerOne);
+            this.Controls.Add(this.btnCptFour);
+            this.Controls.Add(this.btnCptThree);
+            this.Controls.Add(this.btnCptTwo);
+            this.Controls.Add(this.btnCptOne);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Card Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCptOne;
+        private System.Windows.Forms.Button btnCptTwo;
+        private System.Windows.Forms.Button btnCptThree;
+        private System.Windows.Forms.Button btnCptFour;
+        private System.Windows.Forms.Button btnPlayerOne;
+        private System.Windows.Forms.Button btnPlayerTwo;
+        private System.Windows.Forms.Button btnPlayerThree;
+        private System.Windows.Forms.Button btnPlayerFour;
+        private System.Windows.Forms.Button btnBet;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ assignment2-300364433/CardGame/CardGame/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardGame
+{
+    
+    public partial class Form1 : Form
+    {
+        string[] cards = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+        
+        int[] computer = new int[4];
+        int[] player = new int[4];
+
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnBet_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            Random random = new Random();
+
+            if(i == 1)
+            {
+                computer[0] = random.Next(1, 13);
+                player[0] = random.Next(1, 13);
+
+                btnCptOne.Text = cards[computer[0] - 1];
+                btnPlayerOne.Text = cards[player[0] - 1];
+
+            }
+            else if(i == 2)
+            {
+                computer[1] = random.Next(1, 13);
+                player[1] = random.Next(1, 13);
+
+                btnCptTwo.Text = cards[computer[1] - 1];
+                btnPlayerTwo.Text = cards[player[1] - 1];
+            }
+            else if(i == 3)
+            {
+                computer[2] = random.Next(1, 13);
+                player[2] = random.Next(1, 13);
+
+                btnCptThree.Text = cards[computer[2] - 1];
+                btnPlayerThree.Text = cards[player[2] - 1];
+            }
+            else if(i == 4)
+            {
+                computer[3] = random.Next(1, 13);
+                player[3] = random.Next(1, 13);
+
+                btnCptFour.Text = cards[computer[3] - 1];
+                btnPlayerFour.Text = cards[player[3] - 1];
+            }
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            Winner();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            i = 0;
+
+            MessageBox.Show("New Game!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            btnPlayerOne.Text = btnPlayerTwo.Text = btnPlayerThree.Text = btnPlayerFour.Text = null;
+            btnCptOne.Text = btnCptTwo.Text = btnCptThree.Text = btnCptFour.Text = null;
+
+            computer = new int[4];
+            player = new int[4];
+            
+        }
+
+        public void Winner()
+        {
+            double computerSum = 0;
+            double playerSum = 0;
+
+            for(int i = 0; i < 4; i++)
+            {
+                if (computer[i] > 10)
+                {
+                    computerSum += 0.5;
+                }
+                else
+                {
+                    computerSum += computer[i];
+                }
+                
+
+                if (player[i] > 10)
+                {
+                    playerSum += 0.5;
+                }
+                else
+                {
+                    playerSum += player[i];
+                }              
+            }
+
+            if (computerSum < 10)
+            {
+                if (playerSum < 10)
+                {
+                    if (computerSum > playerSum)
+                    {
+                        MessageBox.Show("Computer Wins!","‍Card Game",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    }
+                    else if (computerSum == playerSum)
+                    {
+                        MessageBox.Show("Tie! Player Wins!", "Card Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Player Wins!", "Card Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("You blast! Computer Wins!", "Card Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                if (playerSum < 10)
+                {
+                    MessageBox.Show("Computer Blast! Player Wins!", "Card Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("All blast! Computer Wins!", "Card Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }        
+    }
+}
