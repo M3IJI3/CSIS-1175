f76fec68ee342283139852a7138204a9b0db7ac8
M3IJI3 <majorangeee@gmail.com>
2022-12-05 20:50:42 -0800

    update files

---
+++ Basic/PartialClassExample/PartialClassExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public partial class Person
+    {
+        private string _name;
+    }
+
+    public partial class Person
+    {
+        public void Test()
+        {
+            _name = "123";
+        }
+    }
+}
---
+++ Basic/SealedClassExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    // 密封类: 不能被继承, 但是可以继承于其他类
+    public sealed class Person : Test
+    {
+
+    }
+
+    public class Test
+    {
+
+    }
+
+    
+}
