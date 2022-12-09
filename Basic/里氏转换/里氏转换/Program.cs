4371bd621a3cd81fcd51e8152d5e6e8bc5677cdb
M3IJI3 <majorangeee@gmail.com>
2022-12-08 18:37:44 -0800

    update files

---
+++ Basic/里氏转换/里氏转换/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 里氏转换
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 里氏转换
+            // 1. 子类可以赋值给父类
+            //Student s = new Student();
+            Person p = new Student();
+
+            string str = string.Join("|", new string[] { "1", "2", "3", "4" });
+            Console.WriteLine(str);
+
+
+            // 2. 如果父类中装的是子类对象, 那么可以讲这个父类强转给子类对象
+            //if(p is Student)
+            //{
+            //    Student s = (Student)p;
+            //    s.StudentSayHello();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Conversion Failure!");
+            //}
+
+            Student t = p as Student;
+            t.StudentSayHello();
+            
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public void PersonSayHello()
+        {
+            Console.WriteLine("i am father");
+        }
+    }
+
+    class Student : Person
+    {
+        public void StudentSayHello()
+        {
+            Console.WriteLine("i am student");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public void TeacherSayHello()
+        {
+            Console.WriteLine("i am teacher");
+        }
+    }
+}
---
+++ Basic/里氏转换练习/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 里氏转换练习
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建10个对象, 通过一个循环 去调用他们各自打招呼的方法
+            Person[] p = new Person[10];
+            Random random = new Random();
+
+            for(int i = 0; i < p.Length; i++)
+            {
+                int r = random.Next(1, 7);
+                switch(r)
+                {
+                    case 1: p[i] = new Student(); break;
+                    case 2: p[i] = new Teacher(); break;
+                    case 3: p[i] = new Beauty(); break;
+                    case 4: p[i] = new Playboy(); break;
+                    case 5: p[i] = new Beast(); break;
+                    case 6: p[i] = new Person(); break;
+                }
+            }
+
+            for(int i = 0; i < p.Length; i++)
+            {
+                if (p[i] is Student)
+                {
+                    ((Student)p[i]).StudentSayhi();
+                }
+                else if (p[i] is Teacher)
+                {
+                    ((Teacher)p[i]).TeacherSayHi();
+                }
+                else if (p[i] is Beauty)
+                {
+                    ((Beauty)p[i]).BeautySayHi();
+                }
+                else if (p[i] is Beast)
+                {
+                    ((Beast)p[i]).BeastSayHi();
+                }
+                else if (p[i] is Playboy)
+                {
+                    ((Playboy)p[i]).PlayBoySayHi();
+                }
+                else
+                {
+                    p[i].PersonSayHi();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        public void PersonSayHi()
+        {
+            Console.WriteLine("i am human");
+        }
+    }
+
+    public class Student : Person
+    {
+        public void StudentSayhi()
+        {
+            Console.WriteLine("i am student");
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public void TeacherSayHi()
+        {
+            Console.WriteLine("i am teacher");
+        }
+    }
+
+    public class Beauty : Person
+    {
+        public void BeautySayHi()
+        {
+            Console.WriteLine("i am beauty");
+        }
+    }
+
+    public class Playboy : Person
+    {
+        public void PlayBoySayHi()
+        {
+            Console.WriteLine("i am playboy");
+        }
+    }
+
+    public class Beast : Person
+    {
+        public void BeastSayHi()
+        {
+            Console.WriteLine("i am beast.");
+        }
+    }
+}
